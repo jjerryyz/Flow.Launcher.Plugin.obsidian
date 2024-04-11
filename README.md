a8452c1bc1554be19c1469e4c5f33b3e5ee1e91b
jerryzheng <zheng.jianli@outlook.com>
2024-04-11 22:16:39 +0800

    Add new files and configurations for Obsidian plugin

---
+++ README.md
@@ -0,0 +1,9 @@
+# Open Vscode Recent Folder 
+
+
+## TodoList
+
+- [ ] Command to open System FS Folder
+- [x] Filter Not Exist Folder
+- [ ] Command to git clone And Open Folder
+- [ ] Shift+Enter Case
---
+++ src/index.ts
@@ -0,0 +1,82 @@
+import { Glob } from 'bun'
+import open from 'open'
+import Fuse from 'fuse.js'
+import { Flow } from './lib/flow'
+import logger from './lib/logger'
+
+// The events are the custom events that you define in the flow.on() method.
+const events = ['search'] as const
+type Events = (typeof events)[number]
+
+const flow = new Flow<Events>()
+
+flow.on('query', (params = []) => {
+  const [query] = params as string[]
+
+  if (!query)
+    return flow.showInputHint()
+
+  const commandList = [
+    ['✨new drawing', 'obsidian://advanced-uri?vault=fuckBrain&commandid=obsidian-excalidraw-plugin%253Aexcalidraw-autocreate-newtab'],
+    ['✨new canvas', 'obsidian://advanced-uri?vault=fuckBrain&commandid=canvas%253Anew-file'],
+  ]
+
+  const result: any[] = []
+  commandList.forEach(([title, url]) => {
+    if (title.includes(query)) {
+      result.push({
+        Title: title,
+        Subtitle: '',
+        JsonRPCAction: {
+          method: 'search',
+          parameters: [url],
+          dontHideAfterAction: false,
+        },
+        ContextData: [],
+        IcoPath: 'assets\\app.png',
+        Score: 0,
+      })
+    }
+  })
+
+  try {
+    const glob = new Glob('!attachment/*')
+
+    const files = []
+    for (const file of glob.scanSync('D:/Obsidian/fuckBrain'))
+      files.push({ file })
+
+    const fuze = new Fuse(files, { keys: ['file'] })
+
+    fuze.search(query).forEach(({ item }) => {
+      result.push({
+        Title: item.file,
+        Subtitle: '',
+        JsonRPCAction: {
+          method: 'search',
+          parameters: [`obsidian://open?vault=fuckBrain&file=${encodeURIComponent(item.file)}`],
+          dontHideAfterAction: false,
+        },
+        ContextData: [],
+        IcoPath: 'assets\\app.png',
+        Score: 0,
+      })
+    })
+
+    console.log(JSON.stringify({ result }))
+  }
+  catch (error) {
+    logger.error(error)
+    return flow.showInputHint(JSON.stringify(error))
+  }
+})
+
+flow.on('search', (params) => {
+  logger.info('Search', params)
+  const [url] = params
+
+  if (url)
+    open(url as string)
+})
+
+flow.run()
